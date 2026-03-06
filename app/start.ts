87bbf014f12f608f38b91def22c592348f9fbb2e
Christoph Hennemann <christoph.hennemann@gmail.com>
2026-03-06 16:26:00 +0100

    fix: register Clerk request middleware for SSR auth

--- app/start.ts
+++ app/start.ts
@@ -1,3 +1,6 @@
+import { clerkMiddleware } from "@clerk/tanstack-react-start/server";
 import { createStart } from "@tanstack/react-start";
 
-export const startInstance = createStart(() => ({}));
+export const startInstance = createStart(() => ({
+  requestMiddleware: [clerkMiddleware()],
+}));
